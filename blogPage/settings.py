7f2561b76b84090009009f00e037aaf63a9169dc
Lautaro <lautaro.pastorino@hotmail.com>
2020-05-15 20:53:34 -0300

    ignore y escondo variables

--- blogPage/settings.py
+++ blogPage/settings.py
@@ -1,4 +1,5 @@
 import os
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -7,17 +8,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8m=3t)%u=slz4k55xgha9sdyics1knk1itkhimfio&*)+b4wf2'
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'lautaropastorino.pythonanywhere.com',
-    '127.0.0.1',
-    '192.168.0.10',
-]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
